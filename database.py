e809438b1566dd6ec7ab69cf501826f200001e36
Harvey Shi <harveyyshi@gmail.com>
2018-03-10 12:37:55 -0500

    Adds database handlers and integrates them with server code.

---
+++ database.py
@@ -0,0 +1,25 @@
+def add_heart_rate(input):
+    message = ''
+    code = 200
+    return (message, code, None)
+
+
+def get_hr(input):
+    message = ''
+    code = 200
+    data = [233, 123]
+    return (message, code, data)
+
+
+def get_avg_hr(input):
+    message = ''
+    code = 200
+    data = 2.3
+    return (message, code, data)
+
+
+def get_int_avg(input):
+    message = ''
+    code = 200
+    data = 3.5
+    return (message, code, data)
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import markdown
 from flask import Flask, jsonify, request, render_template
 from validate import val_heart_rate, val_email, val_interval_average
+from database import add_heart_rate, get_hr, get_avg_hr, get_int_avg
 
 app = Flask(__name__, static_url_path='/static')
+
+
+def handle_data(input, validator, action):
+    """Handles API calls
+
+    :param input: input data from request
+    :param validator: validator function to use
+    :param action: database function to apply to data
+    :return: jsonified response
+    """
+
+    if(validator(input)):
+        message = 'ERROR: input was incorrectly formatted.'
+        code = 400
+        data = None
+    else:
+        (message, code, data) = action(input)
+
+    res = {
+        'message': message,
+        'input': input
+    }
+    if(data):
+        res['data'] = data
+    return jsonify(res), code
 
 
 @app.route('/', methods=['GET'])
@@ -15,40 +41,20 @@ def welcome():
 @app.route('/api/heart_rate', methods=['POST'])
 def new_data():
     r = request.get_json()
-    if(val_heart_rate(r)):
-        res = {
-            'message': 'ERROR: input was incorrectly formatted.',
-            'input': r
-        }
-        return jsonify(res), 400
+    return handle_data(r, val_heart_rate, add_heart_rate)
 
 
 @app.route('/api/heart_rate/<user_email>', methods=['GET'])
 def user_data(user_email):
-    if(val_email(user_email)):
-        res = {
-            'message': 'ERROR: input was incorrectly formatted.',
-            'input': user_email
-        }
-        return jsonify(res), 400
+    return handle_data(user_email, val_email, get_hr)
 
 
 @app.route('/api/heart_rate/average/<user_email>', methods=['GET'])
 def average(user_email):
-    if(val_email(user_email)):
-        res = {
-            'message': 'ERROR: input email was incorrectly formatted.',
-            'input': user_email
-        }
-        return jsonify(res), 400
+    return handle_data(user_email, val_email, get_avg_hr)
 
 
 @app.route('/api/heart_rate/interval_average', methods=['POST'])
 def interval_average():
     r = request.get_json()
-    if(val_interval_average(r)):
-        res = {
-            'message': 'ERROR: input data was incorrectly formatted.',
-            'input': r
-        }
-        return jsonify(res), 400
+    return handle_data(r, val_interval_average, get_int_avg)
