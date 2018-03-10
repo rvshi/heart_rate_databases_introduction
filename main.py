efff3c45c0684ee003b29cf26be8fcb7cd1498af
Harvey Shi <harveyyshi@gmail.com>
2018-03-10 11:06:35 -0500

    Adds input validation using jsonschema.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import markdown
 from flask import Flask, jsonify, request, render_template
+from validate import val_heart_rate, val_email, val_interval_average
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -10,3 +11,45 @@ def welcome():
         content = markdown.markdown(f.read())
         return render_template('index.html', content=content)
 
+
+@app.route('/api/heart_rate', methods=['POST'])
+def new_data():
+    r = request.get_json()
+    if(val_heart_rate(r)):
+        res = {
+            'message': 'ERROR: input was incorrectly formatted.',
+            'input': r
+        }
+        return jsonify(res), 400
+
+    
+
+@app.route('/api/heart_rate/<user_email>', methods=['GET'])
+def user_data(user_email):
+    if(val_email(user_email)):
+        res = {
+            'message': 'ERROR: input was incorrectly formatted.',
+            'input': user_email
+        }
+        return jsonify(res), 400
+    
+
+@app.route('/api/heart_rate/average/<user_email>', methods=['GET'])
+def average(user_email):
+    if(val_email(user_email)):
+        res = {
+            'message': 'ERROR: input email was incorrectly formatted.',
+            'input': user_email
+        }
+        return jsonify(res), 400
+    
+
+@app.route('/api/heart_rate/interval_average', methods=['POST'])
+def interval_average():
+    r = request.get_json()
+    if(val_interval_average(r)):
+        res = {
+            'message': 'ERROR: input data was incorrectly formatted.',
+            'input': r
+        }
+        return jsonify(res), 400
---
+++ validate.py
@@ -0,0 +1,62 @@
+from jsonschema import validate, ValidationError
+from dateutil.parser import parse
+
+# Regex from http://emailregex.com
+email_type = {
+    'type': 'string',
+    'pattern': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+}
+
+heart_rate_type = {
+    'type': 'object',
+    'properties': {
+        'user_email': email_type,
+        'user_age': {
+            'type': 'integer',
+            'minimum': 0
+        },
+        'heart_rate': {
+            'type': 'integer',
+            'minimum': 0,
+            'maximum': 500
+        }
+    },
+   	'required': ['user_email', 'user_age', 'heart_rate'],
+   	'additionalProperties': False
+}
+
+interval_average_type = {
+    'type': 'object',
+    'properties': {
+        'user_email': email_type,
+        'heart_rate_average_since': {
+            'type': 'string'
+        }
+    },
+   	'required': ['user_email', 'heart_rate_average_since'],
+   	'additionalProperties': False
+}
+
+def val_heart_rate(input):
+    try:
+        validate(input, heart_rate_type)
+        return False
+    except ValidationError:
+        return True
+
+
+def val_email(input):
+    try:
+        validate(input, email_type)
+        return False
+    except ValidationError:
+        return True
+    
+
+def val_interval_average(input):
+    try:
+        validate(input, interval_average_type)
+        return False
+        parse(input['heart_rate_average_since'])
+    except (ValueError, ValidationError):
+        return True    
