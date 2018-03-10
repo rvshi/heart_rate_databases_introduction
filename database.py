fdb72e589701d9952cb84620d4388e5362d9b662
Harvey Shi <harveyyshi@gmail.com>
2018-03-10 14:14:40 -0500

    Sets up basic database calls.

--- database.py
+++ database.py
@@ -1,66 +1,87 @@
-from pymodm import connect
+"""Database setup and basic functions
+"""
+from pymodm import connect, errors
 import models
 import datetime
 
+mongo_url = 'mongodb://localhost:27017/heart_rate_app'
+connect(mongo_url)
+
 
 def add_heart_rate(input):
-    message = ''
+    message = update_user(**input)
     code = 200
-    return (message, code, None)
+    return (message, None, code)
 
 
-def get_hr(input):
+def get_hr(user_email):
+    try:
+        u = models.User.objects.raw({'_id': user_email}).first()
+        message = '[INFO] heart rate data retrieved for {}.'.format(
+            user_email)
+        data = u.heart_rate
+        return (message, data, 200)
+    except errors.DoesNotExist:
+        message = '[ERROR] user not found'
+        return(message, None, 400)
+
+
+def get_avg_hr(user_email):
     message = ''
-    code = 200
-    data = [233, 123]
-    return (message, code, data)
-
-
-def get_avg_hr(input):
-    message = ''
-    code = 200
     data = 2.3
-    return (message, code, data)
+    return (message, data, 200)
 
 
 def get_int_avg(input):
     message = ''
-    code = 200
     data = 3.5
-    return (message, code, data)
+    return (message, data, 200)
 
 
-def add_heart_rate(email, heart_rate, time):
-    # Get the first user where _id=suyash@suyashkumar.com
-    user = models.User.objects.raw({"_id": "suyash@suyashkumar.com"}).first()
-    # Append the heart_rate to the user's list of heart rates
-    user.heart_rate.append(heart_rate)
-    # append the current time to the user's list of heart rate times
-    user.heart_rate_times.append(time)
-    user.save()  # save the user to the database
+def update_user(user_email, user_age, heart_rate):
+    time_stamp = datetime.datetime.now()
 
-
-def create_user():
-    u = models.User("suyash@suyashkumar.com", 24, [],
-                    [])  # create a new User instance
-    u.heart_rate.append(60)  # add initial heart rate
-    # add initial heart rate time
-    u.heart_rate_times.append(datetime.datetime.now())
-    u.save()  # save the user to the database
+    query_set = models.User.objects.raw({'_id': user_email})
+    if(query_set.count() == 0):
+        user = models.User(user_email, user_age, [heart_rate], [time_stamp])
+        message = '[INFO] Created new user and added heart rate entry.'
+    else:
+        user = query_set.first()
+        user.age = user_age
+        user.heart_rate.append(heart_rate)
+        user.heart_rate_times.append(time_stamp)
+        message = '[INFO] Added heart rate entry.'
+    user.save()
+    return message
 
 
 def print_user(email):
-    # Get the first user where _id=suyash@suyashkumar.com
-    user = models.User.objects.raw({"_id": "suyash@suyashkumar.com"}).first()
-    print(user.email)
-    print(user.heart_rate)
-    print(user.heart_rate_times)
+    try:
+        user = models.User.objects.raw({'_id': email}).first()
+        print(user.email)
+        print(user.age)
+        print(user.heart_rate)
+        print(user.heart_rate_times)
+    except errors.DoesNotExist:
+        print('User not found')
 
 
-if __name__ == "__main__":
-    # open up connection to db
-    connect("mongodb://localhost:27017/heart_rate_app")
-    # create_user() # we should only do this once, otherwise will overwrite
-    # existing user
-    add_heart_rate("suyash@suyashkumar.com", 60, datetime.datetime.now())
-    print_user("suyash@suyashkumar.com")
+def delete_user(email):
+    try:
+        user = models.User.objects.raw({'_id': email}).first()
+        user.delete()
+    except errors.DoesNotExist:
+        print('User not found')
+
+
+if __name__ == '__main__':
+    email = 'me@harveyshi.com'
+    example_user = {'user_email': email,
+                    'user_age': 100,
+                    'heart_rate': 82
+                    }
+
+    print(update_user(**example_user))
+    print(get_hr(email))
+    print_user(email)
+    delete_user(email)
