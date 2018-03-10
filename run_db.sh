f254986e2637f59c532a62f90d2e951205b82971
Harvey Shi <7685901+rvshi@users.noreply.github.com>
2018-03-10 16:09:10 -0500

    Fixes server-side issues. (#3)
    * Minor tweaks to run scripts for production.
    
    * Fixes validation code error.

--- run_db.sh
+++ run_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v $PWD/db:/data/db -p 127.0.0.1:27017:27017 mongo
+sudo docker run -v $PWD/db:/data/db -p 127.0.0.1:27017:27017 mongo
--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo gunicorn --bind 0.0.0.0:8080 --workers 8 main:app
+sudo gunicorn --bind 0.0.0.0:80 --workers 2 main:app
